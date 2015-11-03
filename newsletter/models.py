5a00297b890c96dc636b1d78ee08267f46f7106c
darknawg63 <darknawg63@gmail.com>
2015-11-03 21:48:39 +0100

    Try Django 1.8 Tutorial - 10 of 42 - Admin

--- newsletter/models.py
+++ newsletter/models.py
@@ -5,7 +5,7 @@ class SignUp(models.Model):
     email = models.EmailField()
     full_name = models.CharField(max_length=120, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-    update = models.DateTimeField(auto_now_add=False, auto_now=True )
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True )
 
     def __str__(self):
         return self.email
