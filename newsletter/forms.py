2b4a9282ba8cafa46d9bbf328800d109311c95c5
darknawg63 <darknawg63@gmail.com>
2015-11-04 19:26:05 +0100

    Try Django 1.8 Tutorial - 12 of 42 - Form Validation

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -4,6 +4,16 @@ from .models import SignUp
 
 
 class SignUpForm(forms.ModelForm):
-    class Meta:
-        model = SignUp
-        fields = ['full_name', 'email']
+     class Meta:
+         model = SignUp
+         fields = ['full_name', 'email']
+
+     def clean_email(self):
+         email = self.cleaned_data.get('email')
+         email_base, provider = email.split("@")
+         domain, extension = provider.split('.')
+         if not domain == 'usc':
+             raise forms.ValidationError("Please make sure you use your USC email.")
+         if not extension == "edu":
+             raise forms.ValidationError("Please use a valid .edu email address.")
+         return email
