8907519a80f18da2b9ffcf5d34afff3880dbd150
darknawg63 <darknawg63@gmail.com>
2015-11-04 22:02:54 +0100

    Try Django 1.8 Tutorial - 14 of 42 - Form in a View

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -6,14 +6,14 @@ from .models import SignUp
 class SignUpForm(forms.ModelForm):
      class Meta:
          model = SignUp
-         fields = ['full_name', 'email']
+         fields = ['full_name', 'email'] # This is a list of the fields we want 
 
      def clean_email(self):
          email = self.cleaned_data.get('email')
          email_base, provider = email.split("@")
          domain, extension = provider.split('.')
-         if not domain == 'usc':
-             raise forms.ValidationError("Please make sure you use your USC email.")
+         #if not domain == 'usc':
+         #    raise forms.ValidationError("Please make sure you use your USC email.")
          if not extension == "edu":
              raise forms.ValidationError("Please use a valid .edu email address.")
          return email
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 
+from .forms import SignUpForm
+
 # Create your views here.
 def home(request):
     title = "Welcome"
-    if request.user.is_authenticated():
-        title = "My Title %s" %(request.user)
+    form = SignUpForm(request.POST or None)
     context = {
         "title": title,
+        "form": form,
     }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        # do not use request.POST("full_name") as this bypasses validation and
+        # escaping
+        full_name = form.cleaned_data.get("full_name")
+
+        if not full_name:
+            full_name = "New full name"
+            instance.full_name = full_name
+        instance.save()
+        context = {
+            "title": "Thank you"
+        }
+
     return render(request, "home.html", context)
