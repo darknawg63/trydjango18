085480a1898f5136cf157e9882169739087440a4
darknawg63 <darknawg63@gmail.com>
2015-11-07 21:19:51 +0100

    Exported DB_PASS and SECRET_KEY

--- trydjango18/settings.py
+++ trydjango18/settings.py
@@ -12,18 +12,34 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.exceptions import ImproperlyConfigured
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Handling Key Import Errors
+def get_env_variable(var_name):
+    """ Get the environment variable or return exception """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+# Get ENV VARIABLES key
+ENV_ROLE = get_env_variable('ENV_ROLE')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z(@cnjjm^s6(ya-kz667k@(&sw-+=8qbik621c6c(3d3vz&1x$'
+SECRET_KEY = get_env_variable('TRYDJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+TRYDJANGO_DB_PASS = False
+if ENV_ROLE == 'development':
+    DEBUG = True
+    TRYDJANGO_DB_PASS = get_env_variable('TRYDJANGO_DB_PASS')
 
 ALLOWED_HOSTS = []
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
